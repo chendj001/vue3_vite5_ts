7870d5b473329131fd426b9c0dbd3169a4dcbd5a
chendj001 <719857499@qq.com>
2023-12-08 08:42:35 +0800

    新增演示css3

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
+let routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/AboutView.vue')
+  },
+  {
+    path: '/md',
+    name: 'md',
+    component: () => import('@/md/help.md')
+  }
+]
+const css = [
+  {
+    path: '/css3',
+    name: 'css3',
+    component: () => import('@/views/css3.vue')
+  }
+]
+routes.push(...css)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/views/AboutView.vue')
-    },
-    {
-      path: '/md',
-      name: 'md',
-      component: () => import('@/md/help.md')
-    }
-  ]
+  routes: routes
 })
-
 export default router
---
+++ src/views/css3.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="width: 40px;height: 40px;cursor: pointer;">
+    <Icon>
+      <template #default>🍘</template>
+      <template #fill>🍘</template>
+    </Icon>
+  </div>
+</template>
+
+<script setup lang="ts" name="Css3">
+import Icon from '@/css3/icon'
+</script>
+
+<style scoped></style>
