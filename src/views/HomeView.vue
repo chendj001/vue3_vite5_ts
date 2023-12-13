8282da89743d9ec99e3291ef89854d85ff1a217d
chendj001 <719857499@qq.com>
2023-12-13 16:18:00 +0800

    修改布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,69 +1,121 @@
 <template>
-  <div style="padding: 50px; gap: 10px; display: flex">
+  <div style="padding: 50px 20px; gap: 10px; display: flex; flex-wrap: wrap">
     <div class="grid">
-      <div class="grid-col" style="grid-row:1/span 2; grid-column: 1/span 2 ">
+      <div
+        class="grid-col"
+        style="grid-row: 1 / span 2; grid-column: 1 / span 2"
+      >
         <div
-          style="display: grid;grid-template-columns: repeat(2, 1fr);grid-template-rows: repeat(2, 1fr);gap:4px;padding: 4px;background-color: #fff;">
-          <div style="grid-column: 1/span 1;grid-row: 1/span 1;">
-            <img class="grid-col-img" src="https://avatars.githubusercontent.com/u/146858765?v=4" alt="">
+          style="
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+            gap: 4px;
+            padding: 4px;
+            background-color: #fff;
+          "
+        >
+          <div style="grid-column: 1 / span 1; grid-row: 1 / span 1">
+            <img
+              class="grid-col-img"
+              src="https://avatars.githubusercontent.com/u/146858765?v=4"
+              alt=""
+            />
           </div>
-          <div style="grid-column: 2/span 1;grid-row: 1/span 1;">
-            <img class="grid-col-img" src="https://avatars.githubusercontent.com/u/146858765?v=4" alt="">
+          <div style="grid-column: 2 / span 1; grid-row: 1 / span 1">
+            <img
+              class="grid-col-img"
+              src="https://avatars.githubusercontent.com/u/146858765?v=4"
+              alt=""
+            />
           </div>
-          <div style="grid-column: 1/span 1;grid-row: 2/span 1;">
-            <img class="grid-col-img" src="https://avatars.githubusercontent.com/u/146858765?v=4" alt="">
+          <div style="grid-column: 1 / span 1; grid-row: 2 / span 1">
+            <img
+              class="grid-col-img"
+              src="https://avatars.githubusercontent.com/u/146858765?v=4"
+              alt=""
+            />
           </div>
-          <div style="grid-column: 2/span 1;grid-row: 2/span 1;">
-            <img class="grid-col-img" src="https://avatars.githubusercontent.com/u/146858765?v=4" alt="">
+          <div style="grid-column: 2 / span 1; grid-row: 2 / span 1">
+            <img
+              class="grid-col-img"
+              src="https://avatars.githubusercontent.com/u/146858765?v=4"
+              alt=""
+            />
           </div>
         </div>
       </div>
-
     </div>
-    <div class="grid" style="--col:8;--row:4">
+    <div class="grid" style="--col: 8; --row: 4">
       <div class="grid-col">
-        <img class="avatar"
+        <img
+          class="avatar"
           src="https://p3-pc.douyinpic.com/img/aweme-avatar/tos-cn-avt-0015_a86e56c980b7955312b5702a547a4cf0~c5_300x300.jpeg?from=2956013662"
-          alt="" />
+          alt=""
+        />
       </div>
       <div class="grid-col">
         <a class="grid-link" href="https://www.bilibili.com/" target="_blank">
-          <img class="grid-link-img" src="/bilibili.svg" alt="">
+          <img class="grid-link-img" src="/bilibili.svg" alt="" />
         </a>
       </div>
       <div class="grid-col">
         <a class="grid-link" href="https://juejin.cn/" target="_blank">
-          <img class="grid-link-img" src="/juejin.svg" alt="">
+          <img class="grid-link-img" src="/juejin.svg" alt="" />
         </a>
       </div>
-      <div class="grid-col" style="padding: 3px;background-color: #fff;">
+      <div class="grid-col" style="padding: 3px; background-color: #fff">
         <a class="grid-link" href="https://hellogithub.com/" target="_blank">
-          <img class="grid-link-img" src="/hellogithub.png" alt="">
+          <img class="grid-link-img" src="/hellogithub.png" alt="" />
         </a>
       </div>
       <div class="grid-col" style="grid-column: 1/-1; grid-row: 2/-1">
-        <div class="github-content" style="width: 100%;">
-          <a class="github-item" target="_blank"
-            href="https://www.douyin.com/user/MS4wLjABAAAAwCF9lNamTpx-8ENFQauLVaKucBuRjO3VrRUDWf3BN3s?modal_id=7291961030303862066">🌵大明朱三爷2</a>
+        <div class="github-content" style="width: 100%">
+          <a
+            class="github-item"
+            target="_blank"
+            href="https://www.douyin.com/user/MS4wLjABAAAAwCF9lNamTpx-8ENFQauLVaKucBuRjO3VrRUDWf3BN3s?modal_id=7291961030303862066"
+            >🌵大明朱三爷2</a
+          >
           <div class="github-item">👀 大明指路人</div>
           <div class="github-item">🌱 穿越大秦</div>
         </div>
       </div>
     </div>
-    <div class="grid" style="padding: 0;">
+    <div class="grid" style="padding: 0">
       <div class="grid-col" style="grid-column: 1/-1; grid-row: 1/-1">
-      <img style="object-fit: cover;width: 100%;" src="https://s3.mashiro.top/mstdn/site_uploads/files/000/000/002/original/forest.gif" alt="">
+        <img
+          style="object-fit: cover; width: 100%"
+          src="https://s3.mashiro.top/mstdn/site_uploads/files/000/000/002/original/forest.gif"
+          alt=""
+        />
       </div>
     </div>
     <div class="github">
       <div class="github-header">chendj001/README.md</div>
       <div class="github-content">
-
         <div class="github-item">chmod -R 755</div>
-        <router-link class="github-item" :to="{ name: 'md' }" target="_blank">👀 Md文档</router-link>
-        <router-link class="github-item" :to="{ name: 'vue2Array' }" target="_blank">🌱 数组操作</router-link>
-        <router-link class="github-item" :to="{ name: 'vue2Error' }" target="_blank">💞️ 常见错误</router-link>
-        <router-link class="github-item" :to="{ name: 'vueComponent' }" target="_blank">🎍 组件编写</router-link>
+        <router-link class="github-item" :to="{ name: 'md' }" target="_blank"
+          >👀 Md文档</router-link
+        >
+        <router-link
+          class="github-item"
+          :to="{ name: 'vue2Array' }"
+          target="_blank"
+          >🌱 数组操作</router-link
+        >
+        <router-link
+          class="github-item"
+          :to="{ name: 'vue2Error' }"
+          target="_blank"
+          >💞️ 常见错误</router-link
+        >
+        <router-link
+          class="github-item"
+          :to="{ name: 'vueComponent' }"
+          target="_blank"
+          >🎍 组件编写</router-link
+        >
       </div>
     </div>
   </div>
@@ -143,7 +195,6 @@
       color: rgb(110, 119, 129);
     }
   }
-
 }
 
 .avatar {
